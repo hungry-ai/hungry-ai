f0442f696d73191fd0c61ba0ad966acdbf920b6a
gnehzza <alex.zhongkai.zheng@gmail.com>
2023-03-27 08:35:42 -0400

    Added code to generate nearest neighbor graph and made a minor change in abstract class design and probably fixed a few bugs

--- src/graph/graph_basic_test.py
+++ src/graph/graph_basic_test.py
@@ -11,9 +11,9 @@ def test_graph_basic():
     result = graph_1.add_edge((1,2,1))
     assert None == result
     
-    vertex_1 = graph_1.add_vertex()
-    vertex_2 = graph_1.add_vertex()
-    vertex_3 = graph_1.add_vertex()
+    vertex_1 = graph_1.add_vertex((1,1))
+    vertex_2 = graph_1.add_vertex(("Hi", "Wassup"))
+    vertex_3 = graph_1.add_vertex((2,30000))
     assert 0 == vertex_1
     assert 1 == vertex_2
     assert 2 == vertex_3
