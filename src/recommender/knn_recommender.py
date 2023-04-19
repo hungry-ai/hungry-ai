3f8a09ad88bfdb36b848f0c30029eaa1090bf115
gnehzza <alex.zhongkai.zheng@gmail.com>
2023-04-19 00:05:28 -0400

    added parents dictionary so we can trace the shortest paths

--- src/recommender/knn_recommender.py
+++ src/recommender/knn_recommender.py
@@ -7,6 +7,9 @@ from ..graph import Graph, Vertex, VertexType
 class KNNRecommender(Recommender):
     def __init__(self, graph: Graph) -> None:
         super().__init__(graph)
+        self.last_search_id = None
+        self.parents_dict = None
+        self.distance_dict = None
 
     def predict_rating(self, user_id: str, image_id: str) -> float:
         return 2.5
@@ -19,16 +22,16 @@ class KNNRecommender(Recommender):
 
         recommendations = []
         distance = {src: 0.0}
-        processed = set()
+        parent = dict()
 
         priority_queue = []
-        heapq.heappush(priority_queue, (distance[src], src.id, src.type, src))
+        heapq.heappush(priority_queue, (distance[src], src.id, src.type, src, None))
 
         while len(priority_queue) and len(recommendations) < num_recs:
-            _, _, _, current_vertex = heapq.heappop(priority_queue)
-            if current_vertex in processed:
+            _, _, _, current_vertex, previous_vertex = heapq.heappop(priority_queue)
+            if current_vertex in parent:
                 continue
-            processed.add(current_vertex)
+            parent[current_vertex] = previous_vertex
             for neighbor, weight in self.graph.out_neighbors(current_vertex).items():
                 if (
                     neighbor not in distance
@@ -37,8 +40,13 @@ class KNNRecommender(Recommender):
                     distance[neighbor] = distance[current_vertex] + weight
                     heapq.heappush(
                         priority_queue,
-                        (-distance[neighbor], neighbor.id, neighbor.type, neighbor),
+                        (-distance[neighbor], neighbor.id, neighbor.type, neighbor, current_vertex),
                     )
             if current_vertex.type == VertexType.IMAGE:
                 recommendations.append(current_vertex.id)
+        
+        self.last_search_id = user_id
+        self.parents_dict = parent
+        self.distance_dict = distance
+        
         return recommendations
