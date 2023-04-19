01b325ecf010d29357aa7ca5a798eca616c9077f
gnehzza <121521681+gnehzza@users.noreply.github.com>
2023-04-19 10:43:47 -0400

    added parents dictionary so we can trace the shortest paths (#35)
    * added parents dictionary so we can trace the shortest paths
    
    * changed into helper as discussed
    
    * fixed stuff

--- src/recommender/knn_recommender.py
+++ src/recommender/knn_recommender.py
@@ -11,24 +11,24 @@ class KNNRecommender(Recommender):
     def predict_rating(self, user_id: str, image_id: str) -> float:
         return 2.5
 
-    def get_recommendations(self, user_id: str, num_recs: int) -> list[str]:
+    def get_closest_images(self, user_id: str) -> tuple[list[str], dict[Vertex, Vertex]]:
         # dijkstra
         src = Vertex(user_id, VertexType.USER)
         if src not in self.graph.vertices:
-            return []
+            return [], dict()
 
         recommendations = []
         distance = {src: 0.0}
-        processed = set()
+        parent = dict()
 
         priority_queue = []
-        heapq.heappush(priority_queue, (distance[src], src.id, src.type, src))
+        heapq.heappush(priority_queue, (distance[src], src.id, src.type, src, None))
 
-        while len(priority_queue) and len(recommendations) < num_recs:
-            _, _, _, current_vertex = heapq.heappop(priority_queue)
-            if current_vertex in processed:
+        while len(priority_queue):
+            _, _, _, current_vertex, previous_vertex = heapq.heappop(priority_queue)
+            if current_vertex in parent:
                 continue
-            processed.add(current_vertex)
+            parent[current_vertex] = previous_vertex
             for neighbor, weight in self.graph.out_neighbors(current_vertex).items():
                 if (
                     neighbor not in distance
@@ -37,8 +37,13 @@ class KNNRecommender(Recommender):
                     distance[neighbor] = distance[current_vertex] + weight
                     heapq.heappush(
                         priority_queue,
-                        (-distance[neighbor], neighbor.id, neighbor.type, neighbor),
+                        (-distance[neighbor], neighbor.id, neighbor.type, neighbor, current_vertex),
                     )
             if current_vertex.type == VertexType.IMAGE:
                 recommendations.append(current_vertex.id)
-        return recommendations
+        
+        return (recommendations, parent)
+
+    def get_recommendations(self, user_id: str, num_recs: int) -> list[str]:
+        closest_images, parent = self.get_closest_images(user_id)
+        return closest_images[:num_recs]
