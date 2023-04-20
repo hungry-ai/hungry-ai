7611f2ac638b6ce9ba06a319aac7f07cfd33c345
Alef <alefguendabiaani@gmail.com>
2023-04-20 13:36:46 -0600

    WIP

--- src/recommender/__init__.py
+++ src/recommender/__init__.py
@@ -2,6 +2,7 @@ from .evaluation import reviews_dataset, evaluate_predictions, evaluate_recommen
 from .knn_recommender import KNNRecommender, train_knn
 from .recommender import Recommender
 from .recommender_service import RecommenderService
+from .mf_recommender import MFRecommender, train_mf
 
 __all__ = [
     "reviews_dataset",
@@ -11,4 +12,6 @@ __all__ = [
     "train_knn",
     "Recommender",
     "RecommenderService",
+    "MFRecommender",
+    "train_mf",
 ]
