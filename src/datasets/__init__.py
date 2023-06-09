afb9e9b374a4047ce30353b95e7f70d412a411b2
Cody <cody@Codys-MacBook-Pro.local>
2023-06-09 13:38:02 -0700

    remove unnecessary expose

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,4 +1,3 @@
-from .datasets import get_data_root, get_raw_data_root
 from .yelp_reviews import YelpReviews
 
-__all__ = ["get_data_root", "get_raw_data_root", "YelpReviews"]
+__all__ = ["YelpReviews"]
