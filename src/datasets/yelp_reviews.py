6d93314079b8efd9acb13923c8fe8dfbd3dbbf54
Cody <cody@Codys-MacBook-Pro.local>
2023-06-09 13:34:09 -0700

    add train_test_splidd, k_folds_splidd, all indexes

--- src/datasets/yelp_reviews.py
+++ src/datasets/yelp_reviews.py
@@ -1,8 +1,19 @@
 from pathlib import Path
 import json
 import pandas as pd
+import warnings
 
 from .datasets import get_raw_data_root, get_clean_data_root
+
+
+def add_index(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    index = (
+        df[f"{column}_id"]
+        .drop_duplicates()
+        .reset_index()
+        .rename(columns={"index": f"{column}_index"})
+    )
+    return df.merge(index, on=f"{column}_id")
 
 
 def load_images(images_path: Path) -> pd.DataFrame:
@@ -20,26 +31,125 @@ def gen_images(image_tags_path: Path, images_path: Path) -> pd.DataFrame:
             for label in line["labels"]:
                 images_data.append(
                     {
-                        "photo_id": line["photo_id"],
-                        "business_id": line["business_id"],
-                        "description": label["description"],
-                        "score": label["score"],
+                        "image_id": line["photo_id"],
+                        "restaurant_id": line["business_id"],
+                        "tag_id": label["description"],
+                        "weight": label["score"],
                     }
                 )
 
     images_path.parent.mkdir(parents=True, exist_ok=True)
     images_df = pd.DataFrame(data=images_data)
+
+    images_df = add_index(images_df, "image")
+    images_df = add_index(images_df, "tag")
+
     images_df.to_csv(images_path, index=False)
-    return pd.read_csv(images_path)
+    return load_images(images_path)
 
 
-def load_reviews(reviews_path: Path) -> pd.DataFrame:
-    return pd.read_csv(reviews_path)
+def train_test_splidd(
+    reviews: pd.DataFrame, train_size: float
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    user_weight_cumsum = (
+        reviews.groupby("user_id")["weight"]
+        .sum()
+        .sample(frac=1, random_state=42069)
+        .cumsum()
+    )
+
+    total_weight = user_weight_cumsum.iloc[-1]
+
+    train_users = user_weight_cumsum[user_weight_cumsum <= train_size * total_weight]
+    reviews_train = reviews.merge(
+        train_users, on="user_id", suffixes=("", "_bad")
+    ).drop(columns="weight_bad")
+
+    if reviews_train.empty:
+        warnings.warn("train_test_splidd produced an empty training sample")
+
+    test_users = user_weight_cumsum[user_weight_cumsum > train_size * total_weight]
+    reviews_test = reviews.merge(test_users, on="user_id", suffixes=("", "_bad")).drop(
+        columns="weight_bad"
+    )
+
+    if reviews_test.empty:
+        warnings.warn("train_test_splidd produced an empty testing sample")
+
+    return reviews_train, reviews_test
+
+
+class Fold:
+    def __init__(self, train: pd.DataFrame, test: pd.DataFrame, i: int) -> None:
+        self.train = train
+        self.test = test
+        self.i = i
+
+
+def k_folds_splidd(reviews: pd.DataFrame, n_folds: int) -> list[Fold]:
+    user_weight_cumsum = (
+        reviews.groupby("user_id")["weight"]
+        .sum()
+        .sample(frac=1, random_state=42069)
+        .cumsum()
+    )
+
+    total_weight = user_weight_cumsum.iloc[-1]
+
+    folds = []
+    for i in range(n_folds):
+        is_test_user = (i == 0 or user_weight_cumsum > i / n_folds * total_weight) & (
+            i == n_folds - 1 or user_weight_cumsum <= (i + 1) / n_folds * total_weight
+        )
+
+        train_users = user_weight_cumsum[~is_test_user]
+        train = reviews.merge(train_users, on="user_id", suffixes=("", "_bad")).drop(
+            columns="weight_bad"
+        )
+
+        if train.empty:
+            warnings.warn(
+                f"k_folds_splidd produced an empty training sample on fold {i}"
+            )
+
+        test_users = user_weight_cumsum[is_test_user]
+        test = reviews.merge(test_users, on="user_id", suffixes=("", "_bad")).drop(
+            columns="weight_bad"
+        )
+
+        if test.empty:
+            warnings.warn(
+                f"k_folds_splidd produced an empty testing sample on fold {i}"
+            )
+
+        folds.append(Fold(train, test, i))
+
+    return folds
+
+
+def load_reviews(
+    reviews_train_path: Path,
+    reviews_test_path: Path,
+    reviews_fold_paths: list[tuple[Path, Path]],
+) -> tuple[pd.DataFrame, pd.DataFrame, list[Fold]]:
+    return (
+        pd.read_csv(reviews_train_path),
+        pd.read_csv(reviews_test_path),
+        [
+            Fold(pd.read_csv(fold_train_path), pd.read_csv(fold_test_path), i)
+            for i, (fold_train_path, fold_test_path) in enumerate(reviews_fold_paths)
+        ],
+    )
 
 
 def gen_reviews(
-    yelp_reviews_path: Path, images_df: pd.DataFrame, reviews_path: Path
-) -> pd.DataFrame:
+    yelp_reviews_path: Path,
+    images_df: pd.DataFrame,
+    reviews_train_path: Path,
+    reviews_test_path: Path,
+    reviews_fold_paths: list[tuple[Path, Path]],
+    train_size: float,
+) -> tuple[list[tuple[pd.DataFrame, pd.DataFrame]], pd.DataFrame]:
     if not yelp_reviews_path.exists():
         raise FileNotFoundError(f"{yelp_reviews_path=} does not exist")
 
@@ -50,25 +160,67 @@ def gen_reviews(
             reviews_data.append(
                 {
                     "user_id": line["user_id"],
-                    "business_id": line["business_id"],
+                    "restaurant_id": line["business_id"],
                     "rating": line["stars"],
                     "timestamp": line["date"],
                 }
             )
 
-    reviews_path.parent.mkdir(parents=True, exist_ok=True)
     reviews_df = pd.DataFrame(data=reviews_data)
     reviews_df = reviews_df.merge(
-        images_df[["photo_id", "business_id"]].drop_duplicates(), on="business_id"
+        images_df[["image_id", "image_index", "restaurant_id"]].drop_duplicates(),
+        on="restaurant_id",
     )
+
     weights_df = (
-        reviews_df[["user_id", "business_id"]].value_counts().apply(lambda n: 1 / n)
+        reviews_df[["user_id", "restaurant_id"]].value_counts().apply(lambda n: 1 / n)
     ).rename("weight")
     reviews_df = reviews_df.merge(
-        weights_df, left_on=["user_id", "business_id"], right_index=True
-    ).drop(columns="business_id")
-    reviews_df.to_csv(reviews_path, index=False)
-    return pd.read_csv(reviews_path)
+        weights_df, left_on=["user_id", "restaurant_id"], right_index=True
+    ).drop(columns="restaurant_id")
+
+    train, test = train_test_splidd(reviews_df, train_size=train_size)
+    folds = k_folds_splidd(train, n_folds=len(reviews_fold_paths))
+
+    train = add_index(train, "user")
+    test = add_index(test, "user")
+    for fold in folds:
+        fold.train = add_index(fold.train, "user")
+        fold.test = add_index(fold.test, "user")
+
+    reviews_train_path.parent.mkdir(parents=True, exist_ok=True)
+    train.to_csv(reviews_train_path, index=False)
+
+    reviews_test_path.parent.mkdir(parents=True, exist_ok=True)
+    test.to_csv(reviews_test_path, index=False)
+
+    for fold, (fold_train_path, fold_test_path) in zip(folds, reviews_fold_paths):
+        fold_train_path.parent.mkdir(parents=True, exist_ok=True)
+        fold.train.to_csv(fold_train_path, index=False)
+
+        fold_test_path.parent.mkdir(parents=True, exist_ok=True)
+        fold.test.to_csv(fold_test_path, index=False)
+
+    return load_reviews(reviews_train_path, reviews_test_path, reviews_fold_paths)
+
+
+def get_reviews_paths(
+    reviews_path: Path, n_folds: int
+) -> tuple[Path, Path, list[tuple[Path, Path]]]:
+    prefix, suffix = reviews_path.name.split(".", 1)
+
+    reviews_train_path = reviews_path.parent / f"{prefix}_train.{suffix}"
+    reviews_test_path = reviews_path.parent / f"{prefix}_test.{suffix}"
+
+    reviews_fold_paths = [
+        (
+            reviews_path.parent / f"{prefix}_train_{n_folds}_{i}.{suffix}",
+            reviews_path.parent / f"{prefix}_test_{n_folds}_{i}.{suffix}",
+        )
+        for i in range(n_folds)
+    ]
+
+    return reviews_train_path, reviews_test_path, reviews_fold_paths
 
 
 class YelpReviews:
@@ -82,10 +234,12 @@ class YelpReviews:
         yelp_reviews_path: Path = get_raw_data_root()
         / "yelp_dataset"
         / "yelp_academic_dataset_review.json",
+        train_size: float = 0.8,
+        n_folds: int = 5,
         make_if_not_exists: bool = False,
     ) -> None:
         if images_path.exists():
-            self.images = load_images(images_path)
+            self.images = pd.read_csv(images_path)
         elif make_if_not_exists:
             self.images = gen_images(image_tags_path, images_path)
         else:
@@ -93,15 +247,38 @@ class YelpReviews:
                 f"{images_path=} does not exist, try setting make_if_not_exists=True"
             )
 
-        if reviews_path.exists():
-            self.reviews = load_reviews(reviews_path)
+        reviews_train_path, reviews_test_path, reviews_fold_paths = get_reviews_paths(
+            reviews_path, n_folds
+        )
+        nonexistent_reviews_paths = []
+
+        if not reviews_train_path.exists():
+            nonexistent_reviews_paths.append(reviews_test_path)
+
+        if not reviews_test_path.exists():
+            nonexistent_reviews_paths.append(reviews_test_path)
+
+        nonexistent_reviews_paths = [
+            path
+            for fold_paths in reviews_fold_paths
+            for path in fold_paths
+            if not path.exists()
+        ]
+
+        if len(nonexistent_reviews_paths) == 0:
+            self.reviews_train, self.reviews_test, self.reviews_folds = load_reviews(
+                reviews_train_path, reviews_test_path, reviews_fold_paths
+            )
         elif make_if_not_exists:
-            self.reviews = gen_reviews(yelp_reviews_path, self.images, reviews_path)
+            self.reviews_train, self.reviews_test, self.reviews_folds = gen_reviews(
+                yelp_reviews_path,
+                self.images,
+                reviews_train_path,
+                reviews_test_path,
+                reviews_fold_paths,
+                train_size=train_size,
+            )
         else:
             raise FileNotFoundError(
-                f"{reviews_path=} does not exist, try setting make_if_not_exists=True"
+                f"{nonexistent_reviews_paths=} do not exist, try setting make_if_not_exists=True"
             )
-
-        # join and add weights?
-        # train test splidd?
-        # splidd into folds?
